2744fe9dde5e75fce56862932dac52d7bf288e85
Tony Dang <tony@tonydang.com>
2026-03-29 08:02:16 -0700

    link configs

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -13,3 +13,11 @@ link() {
 	ln -sfn "$src" "$dst"
 	echo "linked: $dst -> $src"
 }
+
+link "$ROOT/.gitconfig" "$HOME/.gitconfig"
+link "$ROOT/.gitignore_global" "$HOME/.gitignore_global"
+link "$ROOT/.iex.exs" "$HOME/.iex.exs"
+link "$ROOT/.zshrc" "$HOME/.zshrc"
+
+link "$ROOT/yabairc" "$HOME/.config/yabai/yabairc"
+link "$ROOT/skhdrc" "$HOME/.config/skhd/skhdrc"
