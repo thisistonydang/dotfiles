bbf00a2d6aa96763c76e0552fd4078a321a14c8a
Tony Dang <tony@tonydang.com>
2026-08-03 09:05:07 -0700

    Add btop.conf and link it into ~/.config/btop.

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -23,6 +23,7 @@ link "$ROOT/starship.toml" "$HOME/.config/starship.toml"
 
 link "$ROOT/config.ghostty" "$HOME/Library/Application Support/com.mitchellh.ghostty/config.ghostty"
 
+link "$ROOT/btop/btop.conf" "$HOME/.config/btop/btop.conf"
 link "$ROOT/btop/themes/rose-pine.theme" "$HOME/.config/btop/themes/rose-pine.theme"
 link "$ROOT/btop/themes/rose-pine-moon.theme" "$HOME/.config/btop/themes/rose-pine-moon.theme"
 link "$ROOT/btop/themes/rose-pine-dawn.theme" "$HOME/.config/btop/themes/rose-pine-dawn.theme"
